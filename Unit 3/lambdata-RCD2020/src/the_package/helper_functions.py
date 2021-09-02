3388679240f9828ed0aaf6e7ba0fba138c7a6e54
RCD2020 <dobertravis@icloud.com>
2021-09-02 13:26:48 -0600

    Added more detail documentation in the lambdata package

--- Unit 3/lambdata-RCD2020/src/the_package/helper_functions.py
+++ Unit 3/lambdata-RCD2020/src/the_package/helper_functions.py
@@ -2,28 +2,41 @@ import pandas as pd
 
 
 class DataFrameInfo:
+    """Simple class that pulls some basic information from a pandas.DataFrame.
+    
+    1. null: total null values in DataFrame
+    2. length: how long the DataFrame is
+    3. shape: shape of the DataFrame"""
 
-    def __init__(self, df):
+    def __init__(self, df: pd.DataFrame):
+        """Initiates DataFrameInfo"""
+
         self.null = df.isnull().sum().sum()
         self.length = len(df)
         self.shape = df.shape
 
 
-def null_count(df):
-    "Takes a pandas.DataFrame and returns the total null values."
+def null_count(df: pd.DataFrame):
+    """Takes a pandas.DataFrame, and returns th total null count in that dataframe."""
 
     return df.isnull().sum().sum()
 
 
-def null_count2(df):
-    "Takes a pandas.DataFrame and returns the total null values."
+def null_count2(df: pd.DataFrame):
+    """Takes a pandas.DataFrame, and returns th total null count in that dataframe.
+    
+    A secondary null_count function that utilizes the DataFrameInfo class to calculate the null count.
+    It is slower than null_count, but oh well. Assignments be like"""
 
     info = DataFrameInfo(df)
 
     return info.null
 
 
-def split_dates(date_series):
+def split_dates(date_series: pd.Series):
+    """Takes a pandas.Series filled with dates in the 'MM/DD/YYYY' format.
+    
+    Returns a pandas.DataFrame with the dates split into three columns; month, day, and year."""
     "Takes a pandas.Series filled with dates in the 'MM/DD/YYYY' format and returns a pandas.DataFrame with the dates split into three columns."
 
     dates = {
