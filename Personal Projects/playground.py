88c612d82b27ba66a87981d991c5c8f794f03cc5
RCD2020 <dobertravis@icloud.com>
2021-09-17 12:30:30 -0600

    we do a little playing

--- Personal Projects/playground.py
+++ Personal Projects/playground.py
@@ -1,22 +1,3 @@
-def retrieve_keys(
-    path='/Users/colby/Documents/Lambda/03 Unit 3/twitterapi.keys'
-):
-    """Retrieves my twitter api keys because .env files won't work"""
-
-    file = open(
-        '/Users/colby/Documents/Lambda/03 Unit 3/twitterapi.keys',
-        'r'
-    )
-
-    data = file.read().split('\n')
-
-    keys = {}
-
-    for x in data:
-        y = x.split('=')
-
-        keys[y[0]] = y[1]
-
-    return keys
-
-print(retrieve_keys())
+test = '000'
+test[0] = '1'
+print(test)
